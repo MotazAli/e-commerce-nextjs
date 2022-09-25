dc9d1609ecf3fb7e5d671d44f0d40c65de972ff9
Motaz Ali <motaz_ali@hotmail.com>
2022-09-25 12:15:45 +0200

    fix load fonts

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,6 +11,8 @@ const Layout = ({children}) => {
           <title>Home | COBRA.Kimonos</title>
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet"></link>
+          <link href="https://fonts.cdnfonts.com/css/gellatio-personal-use" rel="stylesheet"></link>
           </Head>
           <Navbar />
           <main>{children}</main>
